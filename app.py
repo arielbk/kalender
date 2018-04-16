51d20eacc24c6b2a8374ffd4bef073784c202ee7
Ariel Buchwald-Kearney <buchwald.kearney@gmail.com>
2018-04-17 01:09:13 +0200

    Building it up

--- app.py
+++ app.py
@@ -19,3 +19,47 @@ mysql = MySQL(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Register form class - using WTForms
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=6, max=50)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match!')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+# Register - still need to check unique username...
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create a cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute('INSERT INTO users(username, email, password) VALUES(%s, %s, %s)', [username, email, password])
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You are now registered. Proceed to login.', 'success')
+
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# Login
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
