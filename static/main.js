c4a354accdcb6a8ab5001da6dcf44846ef03978c
arielbk <buchwald.kearney@gmail.com>
2018-05-13 03:49:10 +0200

    sidebar animation

--- static/main.js
+++ static/main.js
@@ -1,11 +1,14 @@
 var sideNav = document.getElementById('side-nav');
+var title = document.getElementById('main-title');
 
 function openMenu() {
   sideNav.style.width = '320px';
+  title.style.marginLeft = '354px';
 }
 
 function closeMenu() {
   sideNav.style.width = '0';
+  title.style.marginLeft = '7.5%';
 }
 
 window.addEventListener('click', outsideClick);
